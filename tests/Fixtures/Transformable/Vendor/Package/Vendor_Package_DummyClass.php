ad3081d752c57970db859b2d1316df6156853b7d
Zoltán Borsos <zolli07@gmail.com>
2015-12-20 19:40:37 +0100

    *Fix: .gitignore ignored the dummy test class and test failed

---
+++ tests/Fixtures/Transformable/Vendor/Package/Vendor_Package_DummyClass.php
@@ -0,0 +1,12 @@
+<?php
+
+class Vendor_Package_DummyClass {
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct() {
+        define('TRANSFORMABLE_MODULE_DUMMY_CLASS_REGISTERED', 'TRUE');
+    }
+
+}
